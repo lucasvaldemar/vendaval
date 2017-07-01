0459196c7650ed536d53db439aa0e949071d174d
Leonardo de Souza Martins <leonardosmleo@gmail.com>
2017-07-01 13:38:44 -0300

    tentando ignorar

--- Vendaval/src/main/java/model/produto.java
+++ Vendaval/src/main/java/model/produto.java
@@ -8,11 +8,19 @@ public class produto {
 	@Id
 	private Integer id;
 	private String descricao;
+	private byte ativo;
+	
 	public String getDescricao() {
 		return descricao;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	public byte getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(byte ativo) {
+		this.ativo = ativo;
 	}
 	
 	
