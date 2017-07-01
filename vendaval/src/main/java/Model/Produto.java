e83952ca28d84a4474443c9d21b457384c91896e
Leonardo de Souza Martins <leonardosmleo@gmail.com>
2017-07-01 10:11:11 -0300

    Primeiro Commit

---
+++ vendaval/src/main/java/Model/Produto.java
@@ -0,0 +1,14 @@
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity	
+public class Produto {
+	@Id		
+	private Integer id;
+	private String descricao;
+	
+	
+	
+}
